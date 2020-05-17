908582f9ada411251f5bcd06eac33926870e292f
Shootingfly <xiefei@yolanda.hk>
2020-05-17 23:16:19 +0800

    Initial commit

---
+++ spec/suggest_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Suggest do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/suggest.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Suggest`
+module Suggest
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
